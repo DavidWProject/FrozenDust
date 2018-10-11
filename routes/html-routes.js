0d92c1c60fc1cf8af37fe92526c324b10c590310
David Wong <davidwong@Davids-MBP-2.home>
2018-10-10 23:52:20 -0400

    added some javascript for testing in cms file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,7 +22,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/cms.html"));
   });
 
-  // blog route loads blog.html
+  // Home route loads home.html
   app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
